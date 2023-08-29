1d112ad6dd1410ef2dbd187136a297124b8cd699
lee <lee.jinyoung.k181122@gmail.com>
2023-08-30 03:26:46 +0900

    #16 2단계완료

--- Jinyoung/raw.cpp
+++ Jinyoung/raw.cpp
@@ -9,6 +9,7 @@ using namespace std;
 class Vehicle {
 protected:
     int wheels;
+    string name;
 
 public:
     Vehicle() : wheels(0) {};
@@ -16,7 +17,7 @@ public:
         cout << "바퀴가 " << _wheels << "개인 차량을 만들었습니다." << endl;
     };
 
-    void Drive() { cout << "출발합니다. " << endl; };
+    void Drive(int _speed) { cout << _speed << "km/h 속도로 " << name << "이(가) 출발합니다. " << endl; };
 };
 
 
@@ -24,25 +25,28 @@ class Bicycle : public Vehicle {
 protected:
 
 public:
-    string name = "자전거";
-    Bicycle() {};
-    Bicycle(int _wheels) :Vehicle(_wheels) {
+
+    Bicycle() {  };
+    Bicycle(int _wheels) :Vehicle(_wheels) 
+    {
+        name = "자전거";
     };
 
-    void Drive() { cout << name << "가"; Vehicle::Drive(); };
 };
 
 class Car : public Vehicle {
 protected:
-    int wheels;
-    string name = "자동차";
 
 public:
-    Car() : wheels(0) {};
-    Car(int _wheels) :Vehicle(_wheels) {
+
+    Car() 
+    {
+    };
+    Car(int _wheels) :Vehicle(_wheels) 
+    {
+        name = "자동차";
     };
 
-    void Drive() { cout << name << "가"; Vehicle::Drive(); };
     void Honk() { cout << "빵빵아" << endl; };
 
 };
@@ -55,14 +59,13 @@ public:
 int main() {
     Vehicle vehic(8);
     Bicycle cycle(2);
-    cycle.Drive();
-
     Car car(4);
-    car.Honk();
-    car.Drive();
 
-    //cycle.Honk();  // 주석 제거시 빨간 줄로 오류
-    vehic.Drive();
+    cycle.Drive(10);
+    car.Honk();
+    car.Drive(30);
+
+    //vehic.Drive();
 
 
     return 0;
