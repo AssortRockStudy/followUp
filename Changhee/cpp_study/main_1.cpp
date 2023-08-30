14a5411e8582875523dccf815e9811e3dabdc929
HCH96 <sjrnfl0412@kookmin.ac.kr>
2023-08-31 03:51:34 +0900

    #16 1단계 완료

---
+++ Changhee/cpp_study/main_1.cpp
@@ -0,0 +1,19 @@
+#include "Vehicle.h"
+
+
+
+int main() {
+    Vehicle vehic(8);
+    Bicycle cycle(2);
+    cycle.Drive();
+
+    Car car(4);
+    car.Honk();
+    car.Drive();
+
+    //cycle.Honk();  // 주석 제거시 빨간 줄로 오류
+    vehic.Drive();
+
+
+    return 0;
+}
