d552ad6e1a6ab089f0fa8c14002415844c8004e7
woo <woo060874@gmail.com>
2023-08-31 16:49:13 +0900

    #16 3단계 구현 !!

--- Jin/exrl/Vehicle.h
+++ Jin/exrl/Vehicle.h
@@ -61,59 +61,105 @@ using std::string;
 //};
 
 
+//class Vehicle {
+//private:
+//    int wheels;
+//    string name;
+//
+//public:
+//    Vehicle()
+//        : wheels(0), name("") {};
+//    Vehicle(int _wheels, string _name = "차량")
+//        : wheels(_wheels), name(_name) 
+//    {
+//        cout << "바퀴가 " << _wheels << "개인 " << name << "을(를) 만들었습니다." << endl;
+//        
+//    };
+//
+//protected:
+//    void Drive(int _speed) 
+//    { 
+//        cout << _speed << "km/h 속도로 " << name << "이(가) 출발합니다. " << endl; 
+//    };
+//};
+//
+//class Bicycle : public Vehicle
+//{
+//public:
+//    Bicycle(int _wheels, string _name = "자전거")
+//        : Vehicle(_wheels, _name)
+//    {
+//    }
+//    ~Bicycle()
+//    {}
+//
+//    void Drive(int _speed)
+//    {
+//        Vehicle::Drive(_speed);
+//    }
+//};
+//
+//class Car : public Vehicle
+//{
+//public:
+//    Car(int _wheels, string _name = "자동차")
+//        : Vehicle(_wheels, _name) {}
+//    ~Car()
+//    {}
+//
+//    void Drive(int _speed)
+//    {
+//        Vehicle::Drive(_speed);
+//    }
+//
+//    void Honk()
+//    {
+//        cout << "빵빵" << endl;
+//    }
+//};
+
 class Vehicle {
-private:
+protected:
     int wheels;
     string name;
 
 public:
-    Vehicle()
-        : wheels(0), name("") {};
-    Vehicle(int _wheels, string _name = "차량")
+    Vehicle() : 
+        wheels(0), name("") {};
+
+    Vehicle(int _wheels, string _name = "차량") 
         : wheels(_wheels), name(_name) 
     {
         cout << "바퀴가 " << _wheels << "개인 " << name << "을(를) 만들었습니다." << endl;
-        
     };
 
-protected:
-    void Drive(int _speed) 
-    { 
-        cout << _speed << "km/h 속도로 " << name << "이(가) 출발합니다. " << endl; 
-    };
+public:
+    virtual void Drive()
+    { cout << name << "이 출발합니다. " << endl; };
 };
 
 class Bicycle : public Vehicle
 {
 public:
-    Bicycle(int _wheels, string _name = "자전거")
-        : Vehicle(_wheels, _name)
-    {
-    }
-    ~Bicycle()
-    {}
+    Bicycle(int _wheels)
+        : Vehicle(_wheels, "자전거") {};
 
-    void Drive(int _speed)
-    {
-        Vehicle::Drive(_speed);
-    }
+    ~Bicycle() {};
+
+public:
+    virtual void Drive() override
+    { cout << name << "로 페달을 밟습니다. " << endl; };
 };
 
 class Car : public Vehicle
 {
 public:
-    Car(int _wheels, string _name = "자동차")
-        : Vehicle(_wheels, _name) {}
-    ~Car()
-    {}
+    Car(int _wheels)
+        : Vehicle(_wheels, "자동차") {};
 
-    void Drive(int _speed)
-    {
-        Vehicle::Drive(_speed);
-    }
+    ~Car() {};
+public:
+    virtual void Drive() override
+    { cout << name << "의 악셀을 누릅니다. " << endl; };
 
-    void Honk()
-    {
-        cout << "빵빵" << endl;
-    }
 };
--- Jin/exrl/main_inheritation.cpp
+++ Jin/exrl/main_inheritation.cpp
@@ -16,18 +16,40 @@
 //    return 0;
 //}
 
+//#include "Vehicle.h"
+//
+//int main() {
+//    Vehicle vehic(8);
+//    Bicycle cycle(2);
+//    Car car(4);
+//
+//    cycle.Drive(10);
+//    car.Honk();
+//    car.Drive(30);
+//
+//    // vehic.Drive(); // 주석 제거시 빨간 줄로 오류
+//
+//    return 0;
+//}
+
+
 #include "Vehicle.h"
+#include <vector>
 
-int main() {
-    Vehicle vehic(8);
-    Bicycle cycle(2);
-    Car car(4);
+int main()
+{
+    Vehicle* vehic = new Vehicle(8);
+    Bicycle* cycle = new Bicycle(2);
+    Car* car = new Car(4);
 
-    cycle.Drive(10);
-    car.Honk();
-    car.Drive(30);
+    std::vector<Vehicle*> vehicles;
 
-    // vehic.Drive(); // 주석 제거시 빨간 줄로 오류
+    vehicles.push_back(vehic);
+    vehicles.push_back(cycle);
+    vehicles.push_back(car);
 
-    return 0;
+    for (int i = 0; i < vehicles.size(); i++) {
+        vehicles[i]->Drive();
+    }
+
 }
