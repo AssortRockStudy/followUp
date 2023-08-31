c63a2f8cbbe2bbf20483d06a27918ee4d07d4501
woo <woo060874@gmail.com>
2023-08-31 16:14:26 +0900

    #16 2단계 성공 ! 부모 클래스에서 초기화 해준 변수를 생성자에서 한 번 더 대입을 함... (실수) 부모 클래스의 protected 함수를 자식 클래스에서 호출하는 방법을 깨달음.

--- Jin/exrl/Vehicle.h
+++ Jin/exrl/Vehicle.h
@@ -1,59 +1,119 @@
 #include <iostream>
+#include <string>
 using std::cout;
 using std::endl;
+using std::string;
+
+
+//class Vehicle {
+//protected:
+//    int wheels;
+//
+//public:
+//    Vehicle()
+//        : wheels(0) {};
+//
+//    Vehicle(int _wheels)
+//        : wheels(_wheels) 
+//    {
+//        cout << "바퀴가 " << _wheels << "개인 차량을 만들었습니다." << endl;
+//    };
+//
+//    void Drive()
+//    { cout << "출발합니다. " << endl; };
+//};
+//
+//
+//class Bicycle : public Vehicle
+//{
+//public:
+//    Bicycle()
+//        {};
+//    Bicycle(int _wheels)
+//        : Vehicle(_wheels) {}
+//    ~Bicycle() {}
+//
+//    void Drive()
+//    {
+//        cout << "자전거가 출발합니다." << endl;
+//    }
+//};
+//
+//class Car : public Vehicle
+//{
+//public:
+//    Car()
+//    {};
+//    Car(int _wheels)
+//        : Vehicle(_wheels) {}
+//    ~Car() {}
+//
+//    void Drive()
+//    {
+//        cout << "자동차가 출발합니다." << endl;
+//    }
+//
+//    void Honk()
+//    {
+//        cout << "빵빵" << endl;
+//    }
+//
+//};
 
 
 class Vehicle {
-protected:
+private:
     int wheels;
+    string name;
 
 public:
     Vehicle()
-        : wheels(0) {};
-
-    Vehicle(int _wheels)
-        : wheels(_wheels) 
+        : wheels(0), name("") {};
+    Vehicle(int _wheels, string _name = "차량")
+        : wheels(_wheels), name(_name) 
     {
-        cout << "바퀴가 " << _wheels << "개인 차량을 만들었습니다." << endl;
+        cout << "바퀴가 " << _wheels << "개인 " << name << "을(를) 만들었습니다." << endl;
+        
     };
 
-    void Drive()
-    { cout << "출발합니다. " << endl; };
+protected:
+    void Drive(int _speed) 
+    { 
+        cout << _speed << "km/h 속도로 " << name << "이(가) 출발합니다. " << endl; 
+    };
 };
-
 
 class Bicycle : public Vehicle
 {
 public:
-    Bicycle()
-        {};
-    Bicycle(int _wheels)
-        : Vehicle(_wheels) {}
-    ~Bicycle() {}
-
-    void Drive()
+    Bicycle(int _wheels, string _name = "자전거")
+        : Vehicle(_wheels, _name)
     {
-        cout << "자전거가 출발합니다." << endl;
+    }
+    ~Bicycle()
+    {}
+
+    void Drive(int _speed)
+    {
+        Vehicle::Drive(_speed);
     }
 };
 
 class Car : public Vehicle
 {
 public:
-    Car()
-    {};
-    Car(int _wheels)
-        : Vehicle(_wheels) {}
-    ~Car() {}
+    Car(int _wheels, string _name = "자동차")
+        : Vehicle(_wheels, _name) {}
+    ~Car()
+    {}
 
-    void Drive()
+    void Drive(int _speed)
     {
-        cout << "자동차가 출발합니다." << endl;
+        Vehicle::Drive(_speed);
     }
 
     void Honk()
     {
         cout << "빵빵" << endl;
     }
-
 };
--- Jin/exrl/main_inheritation.cpp
+++ Jin/exrl/main_inheritation.cpp
@@ -1,17 +1,33 @@
+//#include "Vehicle.h"
+//
+//int main() {
+//    Vehicle vehic(8);
+//    Bicycle cycle(2);
+//    cycle.Drive();
+//
+//    Car car(4);
+//    car.Honk();
+//    car.Drive();
+//
+//    //cycle.Honk();  // 주석 제거시 빨간 줄로 오류
+//    vehic.Drive();
+//
+//
+//    return 0;
+//}
+
 #include "Vehicle.h"
 
 int main() {
     Vehicle vehic(8);
     Bicycle cycle(2);
-    cycle.Drive();
-
     Car car(4);
+
+    cycle.Drive(10);
     car.Honk();
-    car.Drive();
+    car.Drive(30);
 
-    //cycle.Honk();  // 주석 제거시 빨간 줄로 오류
-    vehic.Drive();
-
+    // vehic.Drive(); // 주석 제거시 빨간 줄로 오류
 
     return 0;
 }
